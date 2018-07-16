7ef40733b33446ebeba24c0c40fd91199917432c
OHSEJIN <jinio88@gmail.com>
2018-07-17 08:16:40 +0900

    소수 구하기

---
+++ src/ch02/PrimeNumber.java
@@ -0,0 +1,31 @@
+package ch02;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int counter = 0;
+        int ptr = 0;  // 소수갯수
+        int[] prime = new int[500];
+
+        prime[ptr++] = 2;
+        prime[ptr++] = 3;
+
+        for(int n=5;n<=1000;n+=2) {
+            boolean flag = false;
+            for(int i=1;prime[i]*prime[i]<=n;i++) {
+                counter += 2;
+                if(n%prime[i]==0) {
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag) {
+                prime[ptr++] = n;
+                counter++;
+            }
+        }
+        for(int i=0;i<ptr;i++) {
+            System.out.println(prime[i]);
+        }
+
+    }
+}
