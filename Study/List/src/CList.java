af36d61650f974a4b8a438af41b608dcd297804c
오세진 <jinioh88@gmail.com>
2018-07-28 00:31:32 +0900

    원형 리스트

---
+++ Study/List/src/CList.java
@@ -0,0 +1,44 @@
+import java.util.NoSuchElementException;
+
+public class CList <E> {
+    private Node last;
+    private int size;
+    public CList() {
+        last = null;
+        size = 0;
+    }
+
+    public void insert(E newItem) {
+        Node newNode = new Node(newItem, null);
+        if(last==null) {
+            newNode.setNext(newNode);
+            last = newNode;
+            // last가 가리키는 노드의 다음 노드가 newNode가 되도록
+            // last -> node1 -> newNode -> node2 -> ...
+        } else {
+            newNode.setNext(last.getNext());
+            last.setNext(newNode);
+        }
+        size++;
+    }
+
+    public Node delete() {
+        if(isEmpty())
+            throw new NoSuchElementException();
+        Node x = last.getNext();   // 첫노드
+        if(x==last)
+            last = null;
+        else {
+            last.setNext(x.getNext());
+            x.setNext(null);
+        }
+        size--;
+        return x;
+    }
+
+    private boolean isEmpty() {
+        if(last.getNext()==null)
+            return true;
+        return false;
+    }
+}
