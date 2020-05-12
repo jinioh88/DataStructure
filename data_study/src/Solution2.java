dd63ffffa18892bf9a92fc754ae8b40117e7a03b
jinioh88 <jinioh88@gmail.com>
2020-05-12 16:16:26 +0900

    완주하지 못한 선수

---
+++ data_study/src/Solution2.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution2 {
+    public static String solution(String[] participant, String[] completion) {
+        List<String> partList = new ArrayList<>(Arrays.asList(participant));
+        List<String> comList = new ArrayList<>(Arrays.asList(completion));
+
+        partList.removeIf(comList::contains);
+
+        String answer = partList.get(0);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] part = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] comp = {"josipa", "filipa", "marina", "nikola"};
+        System.out.println(solution(part, comp));
+    }
+}
