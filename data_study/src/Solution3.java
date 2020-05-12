b9fa5eecb1a37075e8c22e2667f289c2537986ea
jinioh88 <jinioh88@gmail.com>
2020-05-12 16:37:56 +0900

    오류수정

--- data_study/src/Solution3.java
+++ data_study/src/Solution3.java
@@ -6,9 +6,9 @@ public class Solution3 {
     public static boolean solution(String[] phone_book) {
         boolean answer = true;
 
-        List<String> phones = new ArrayList<>(Arrays.asList(phone_book));
-
         for(int i = 0; i < phone_book.length; i++) {
+            List<String> phones = new ArrayList<>(Arrays.asList(phone_book));
+
             String temp = phone_book[i];
             phones.removeIf(p -> p.equals(temp));
             if(phones.stream().anyMatch(p -> p.startsWith(temp))) {
